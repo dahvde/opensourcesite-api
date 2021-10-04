60cce7855bed5ddd2b48df6b5a80bc5f1c8d0414
david miller <owlsensation@gmail.com>
2021-10-03 21:13:15 -0700

    Added error messaging to main post route

--- routes/main.js
+++ routes/main.js
@@ -6,14 +6,54 @@ router.get("/", async (req, res) => {
     res.json(await DATA.find())
 })
 
-router.post("/", async (req, res) => {
-   const {author, title, thumbnail} = req.body;
+router.post("/", validParams, async (req, res) => {
+  try {
+    const {author, title, thumbnail} = req.body;
     const test = DATA({
     title,
     thumbnail,
     author,
     })
-    await test.save()
+    const saveResponse = await test.save()
+    res.send(saveResponse)
+  } catch (err) {
+    res.status(400).json({msg: err, status: 400})
+  }
 })
+
+function validParams(req, res, next) {
+  try {
+    const {author, title, thumbnail} = req.body;
+    const messages = {
+      "author": ["Author", null],
+      "title": ["Title", null],
+      "thumbnail": ["Thumnail", null]
+    };
+
+    let message_filter = [
+      messages["author"][Boolean(author) * 1],
+      messages["title"][Boolean(title) * 1],
+      messages["thumbnail"][Boolean(thumbnail) * 1]
+    ].filter((i) => i);
+
+    if (message_filter.length) {
+      if (message_filter.length - 1) {
+        message_filter = lastIndexPrepend(message_filter)
+      }
+      let concat_message = message_filter.join(", ");
+      throw `${concat_message} not present in body`;
+    }
+    next();
+  } catch (err) {
+    res.status(400).json({msg: err, status: 400});
+  }
+}
+
+function lastIndexPrepend(values, pre_pendstr) {
+  const last_index = values.length - 1
+  const last_message = values[last_index]
+  values[last_index] = "and " + last_message
+  return values
+}
 
 module.exports = router
